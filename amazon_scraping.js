2fe3f858a2fcaeadd6fcab31662944a3780446f7
Alex Giraldo <gir15002@byui.edu>
2024-03-20 14:29:29 -0600

    new modified

--- amazon_scraping.js
+++ amazon_scraping.js
@@ -1,44 +1,47 @@
 // Importing necessary modules
-import puppeteer from "puppeteer";
-import fs from "fs/promises";
+import puppeteer from "puppeteer"; // Importing Puppeteer for web scraping
+import fs from "fs/promises"; // Helps to write date in JSON file 
 
 // Function to scrape Amazon Best Sellers Books data
 async function amazonBestSellersBooks() {
-    let browser;
-    let extractionTimestamp;
+    let browser; // Declaring a variable to hold the browser instance
+    let extractionTimestamp; // Declaring a variable to hold the extraction timestamp
 
-    try {
+    try { // Try block to handle potential errors
         // Launching Puppeteer and creating a new page
         browser = await puppeteer.launch({
-            headless: false, // Set to true for headless mode
+            headless: false, // Setting headless mode to false for browser visibility
+            //Slow down the Puppeteer operations by a specified amount of milliseconds. 
+            // Useful for debugging and observing the behavior of automated browser interactions in a more human-readable pace.
+            slowMo: 1000, 
         });
-        const page = await browser.newPage();
+        const page = await browser.newPage(); // Creating a new page instance
 
         // Navigating to the Amazon Best Sellers Books page and taking a screenshot
         await page.goto('https://www.amazon.com/gp/bestsellers/books/ref=zg_bs_nav_books_0');
-        await page.screenshot({ path: 'amazon.png' });
+        await page.screenshot({ path: 'amazon.png' }); // Taking a screenshot of the page
 
         // Capturing the timestamp for data extraction
         extractionTimestamp = new Date().toLocaleString();
 
         // Extracting data from the page using evaluate function
-        const results = await page.evaluate((extractionTimestamp) => {
+        const results = await page.evaluate((extractionTimestamp) => { // Executing code in the browser context
             // Selecting all book elements on the page
             const books = document.querySelectorAll('._cDEzb_iveVideoWrapper_JJ34T');
             
             // Mapping each book element to extract relevant information
             const data = [...books].map(book => {
-                const bookTitle = book.querySelector('._cDEzb_p13n-sc-css-line-clamp-1_1Fn1y').innerText;
-                const sellerElement = book.querySelector('.a-size-small');
-                const seller = sellerElement ? sellerElement.textContent.trim() : null;
-                const ratingElement = book.querySelector('.a-icon-row .a-icon-alt');
-                const rating = ratingElement ? ratingElement.textContent.trim() : null;
-                const numReviewsElement = book.querySelector('.a-icon-row .a-size-small');
-                const numReviews = numReviewsElement ? numReviewsElement.textContent.trim() : null;
-                const paperTypeElement = book.querySelector('.a-size-small.a-color-secondary.a-text-normal');
-                const bookFormat = paperTypeElement ? paperTypeElement.textContent.trim() : null;
-                const priceElement = book.querySelector('._cDEzb_p13n-sc-price_3mJ9Z');
-                const price = priceElement ? priceElement.textContent.trim() : null;
+                const bookTitle = book.querySelector('._cDEzb_p13n-sc-css-line-clamp-1_1Fn1y').innerText; // Extracting book title
+                const sellerElement = book.querySelector('.a-size-small'); // Extracting seller element
+                const seller = sellerElement ? sellerElement.textContent.trim() : null; // Extracting seller information
+                const ratingElement = book.querySelector('.a-icon-row .a-icon-alt'); // Extracting rating element
+                const rating = ratingElement ? ratingElement.textContent.trim() : null; // Extracting rating information
+                const numReviewsElement = book.querySelector('.a-icon-row .a-size-small'); // Extracting number of reviews element
+                const numReviews = numReviewsElement ? numReviewsElement.textContent.trim() : null; // Extracting number of reviews
+                const paperTypeElement = book.querySelector('.a-size-small.a-color-secondary.a-text-normal'); // Extracting paper type element
+                const bookFormat = paperTypeElement ? paperTypeElement.textContent.trim() : null; // Extracting paper type information
+                const priceElement = book.querySelector('._cDEzb_p13n-sc-price_3mJ9Z'); // Extracting price element
+                const price = priceElement ? priceElement.textContent.trim() : null; // Extracting price information
 
                 // Returning an object with extracted data for each book
                 return {
@@ -66,10 +69,10 @@ async function amazonBestSellersBooks() {
 
         // Logging success message with the filename
         console.log(`Data extraction and export successful. File saved as: ${filename}`);
-    } catch (error) {
+    } catch (error) { // Catching and handling errors
         // Handling errors and logging error message
         console.error('An error occurred:', error.message);
-    } finally {
+    } finally { // Executing cleanup code
         // Closing the browser instance in the finally block
         if (browser) {
             await browser.close();
